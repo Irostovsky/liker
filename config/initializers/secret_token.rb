afae054deaf9e62a8a304da43f4b3ef80c207c17
Ivan Rostovsky <ir@anahoret.com>
2011-09-23 14:17:25 +0300

    1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Liker::Application.config.secret_token = 'd2f2991191722b4480195b876854193a513b9fa2400be008e1b6d9140e61b4dff8fb97cf8215856fe588269f9dd401688defbfe600990616afded2d2d93dbad3'
